d2ad7d57f27c5a5ae899ae5bd47ddcc1400b769f
Lahari Nagaraj <laharinagaraj9876@gmail.com>
2025-07-23 12:52:34 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="border-t border-gray-700/50 pt-6 text-center text-sm text-gray-400 space-y-1">
         <p className="text-gray-300 font-medium">Developed by</p>
         <p className="text-cyan-400 font-semibold">
-          Dhruv Rathi &nbsp;|&nbsp; Lahari Priya N &nbsp;|&nbsp; Rahita H Puthran
+          Dhruv Rathi &nbsp;| Lahari Priya N &nbsp;| Rahita H Puthran
         </p>
         <p className="text-gray-400">Software Development SIG, IEEE UVCE</p>
       </div>
